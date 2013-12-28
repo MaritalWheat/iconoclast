f2841b6d32bc5693eb872548a32eafeeba0c27f7
Justin Smith <jds0102@gmail.com>
2013-12-28 09:23:12 -0800

    Added and changed things

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager singleton;
+
+	public GameObject characterPrefab;
+
+	Squad squadA, squadB;
+
+	// Use this for initialization
+	void Start () {
+		if (singleton == null) {
+			singleton = this;
+		}
+		squadA = this.gameObject.AddComponent<Squad> ();
+		squadB = this.gameObject.AddComponent<Squad> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Project/Assets/Scripts/Squad.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Squad : MonoBehaviour {
+
+	public const int SQUAD_SIZE = 5;
+
+	List<GameObject> characters;
+
+	// Use this for initialization
+	void Start () {
+		characters = new List<GameObject>();
+		for (int i = 0; i < SQUAD_SIZE; i++) {
+			Debug.Log("Character " + i);
+			characters.Add((GameObject)GameObject.Instantiate(GameManager.singleton.characterPrefab));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
