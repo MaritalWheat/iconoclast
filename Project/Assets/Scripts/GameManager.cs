6a7ef339a7c67ed67ab71494ef0d9818475a79ca
Justin Smith <jds0102@gmail.com>
2013-12-28 11:06:45 -0800

    Added a turn that incrememnts when the squad is finished making its move

--- Project/Assets/Scripts/GameManager.cs
+++ Project/Assets/Scripts/GameManager.cs
@@ -8,18 +8,27 @@ public class GameManager : MonoBehaviour {
 	public GameObject characterPrefab;
 
 	Squad squadA, squadB;
-
+	int totalTurns = 0;
 	// Use this for initialization
 	void Start () {
 		if (singleton == null) {
 			singleton = this;
 		}
 		squadA = this.gameObject.AddComponent<Squad> ();
-		squadB = this.gameObject.AddComponent<Squad> ();
+		squadA.startTurn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Wait for players to finish turns
+		if(!squadA.isTurnComplete()) {
+			//waiting
+		} else {
+			//Process Turns
+			totalTurns ++;
+			Debug.Log("This is where we process what happened for turn number " + totalTurns);
+			//Start the next round of turns
+			squadA.startTurn();
+		}
 	}
 }
--- Project/Assets/Scripts/Squad.cs
+++ Project/Assets/Scripts/Squad.cs
@@ -8,11 +8,12 @@ public class Squad : MonoBehaviour {
 
 	List<GameObject> characters;
 
+	bool turnComplete = true;
+
 	// Use this for initialization
 	void Start () {
 		characters = new List<GameObject>();
 		for (int i = 0; i < SQUAD_SIZE; i++) {
-			Debug.Log("Character " + i);
 			characters.Add((GameObject)GameObject.Instantiate(GameManager.singleton.characterPrefab));
 		}
 	}
@@ -21,4 +22,21 @@ public class Squad : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnGUI () {
+		if (!turnComplete) {
+			if (GUI.Button(new Rect(0, 200, 100, 50), "Finish Turn")) {
+				turnComplete = true;
+			}
+		}
+	}
+
+	public void startTurn() {
+		turnComplete = false;
+	}
+
+	public bool isTurnComplete() {
+		return turnComplete;
+	}
+
 }
